5680440e7356ea85175b175b6bfcb789170f31ae
nicholaspohlmann <pohlmann.nick@gmail.com>
2021-06-04 19:57:57 -0400

    Added computation and the output statement

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,10 +14,15 @@ public class App {
 
         String firstNumString = myApp.readFirstNum();
         String secondNumString = myApp.readSecondNum();
-        Integer firstNum = myApp.convertFirstNum(firstNumString);
-        Integer secondNum = myApp.convertSecondNum(secondNumString);
+        int firstNum = myApp.convertFirstNum(firstNumString);
+        int secondNum = myApp.convertSecondNum(secondNumString);
+        int sum = myApp.computeSum(firstNum, secondNum);
+        int difference = myApp.computeDifference(firstNum, secondNum);
+        int product = myApp.computeProduct(firstNum, secondNum);
+        int quotient = myApp.computeQuotient(firstNum, secondNum);
+        String outputString = myApp.generateOutputString(firstNum, secondNum, sum, difference, product, quotient);
+        myApp.printOutput(outputString);
     }
-
     private String readFirstNum() {
         System.out.print("What is the first number? ");
         String firstNum = in.nextLine();
@@ -39,4 +44,32 @@ public class App {
         int secondNum = Integer.parseInt(secondNumString);
         return secondNum;
     }
+
+    private Integer computeSum(int firstNum, int secondNum) {
+        int sum = firstNum + secondNum;
+        return sum;
+    }
+
+    private Integer computeDifference(int firstNum, int secondNum) {
+        int difference = firstNum - secondNum;
+        return difference;
+    }
+
+    private Integer computeProduct(int firstNum, int secondNum) {
+        int product = firstNum * secondNum;
+        return product;
+    }
+
+    private Integer computeQuotient(int firstNum, int secondNum) {
+        int quotient = firstNum / secondNum;
+        return quotient;
+    }
+
+    private String generateOutputString(int firstNum, int secondNum, int sum, int difference, int product, int quotient) {
+        return String.format("%d + %d = %d \n%d - %d = %d \n%d * %d = %d\n%d / %d = %d", firstNum, secondNum, sum, firstNum, secondNum, difference, firstNum, secondNum, product, firstNum, secondNum, quotient);
+    }
+
+    private void printOutput(String outputString) { System.out.println(outputString); }
 }
+
+
